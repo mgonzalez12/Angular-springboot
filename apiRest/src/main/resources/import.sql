a0916c751485dc096ffdf4091d30029d9ec37fc8
mgonzalez12 <ing.marcosgonzalez1@gmail.com>
2021-11-02 22:13:06 -0500

    Paginator en spring boot and angular

--- apiRest/src/main/resources/import.sql
+++ apiRest/src/main/resources/import.sql
@@ -7,5 +7,12 @@ INSERT INTO clientes(nombre,apellido,email,create_at) values("Magma","Vlissides"
 INSERT INTO clientes(nombre,apellido,email,create_at) values("Tornado","Gosling","gosling@mail.com",'2021-08-28');
 INSERT INTO clientes(nombre,apellido,email,create_at) values("Jade","Doe","doejade@mail.com",'2021-08-28');
 
-
+INSERT INTO clientes(nombre,apellido,email,create_at) values("Marcos","Gonzalez","marc1@mail.com",'2021-08-28');
+INSERT INTO clientes(nombre,apellido,email,create_at) values("Luis","Rojas","lrojas1@mail.com",'2021-08-28');
+INSERT INTO clientes(nombre,apellido,email,create_at) values("Jose","Helm","hel1m@mail.com",'2021-08-28');
+INSERT INTO clientes(nombre,apellido,email,create_at) values("Ralp","Johnson","jhons1@mail.com",'2021-08-28');
+INSERT INTO clientes(nombre,apellido,email,create_at) values("Alex","Roe","roe1@mail.com",'2021-08-28');
+INSERT INTO clientes(nombre,apellido,email,create_at) values("Magma","Vlissides","bvis1@mail.com",'2021-08-28');
+INSERT INTO clientes(nombre,apellido,email,create_at) values("Tornado","Gosling","gosling1@mail.com",'2021-08-28');
+INSERT INTO clientes(nombre,apellido,email,create_at) values("Jade","Doe","doejade1@mail.com",'2021-08-28');
 
